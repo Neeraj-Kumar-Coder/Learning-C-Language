37c3076b3fefbae443e49ffe0396e71b012b8ebb
Neeraj Kumar <nk7075034@gmail.com>
2020-11-28 19:53:16 +0530

    Created an important function (incomplete) for array manupulation

---
+++ Array_Arranging_Function.c
@@ -0,0 +1,38 @@
+// Ascending order arranger
+int *ascendingOrderArranger(int arr[], int size)
+{
+    ;
+}
+
+// Shortest Number Finder
+int shortestNumberFinder(int arr[], int size)
+{
+    int temp = arr[0];
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (temp > arr[i])
+        {
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
+// Descending order arranger
+int *descendingOrderArranger(int arr[], int size)
+{
+    ;
+}
+
+// Highest Number Finder
+int highestNumberFinder(int arr[], int size)
+{
+    int temp = arr[0];
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (temp < arr[i])
+        {
+            temp = arr[i];
+        }
+    }
+    return temp;
+}
---
+++ Test_Program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Array_Arranging_Function.c"
+
+int main(int argc, char const *argv[])
+{
+    int number_of_terms;
+    printf("How many elements does your sequence contains? : ");
+    scanf("%d", &number_of_terms);
+
+    int arr[number_of_terms - 1];
+
+    printf("****Enter the numbers in ascending order****\n");
+    for (int i = 0; i < number_of_terms; i++)
+    {
+        printf("Enter the %d element of the array: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("The shortest number is: %d\n", shortestNumberFinder(arr, number_of_terms));
+    printf("The highest number is: %d\n", highestNumberFinder(arr, number_of_terms));
+    return 0;
+}

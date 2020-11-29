e2a6cb284adcf2fb9e50d7671d0a7eb94c67b8de
Neeraj Kumar <nk7075034@gmail.com>
2020-11-29 17:56:58 +0530

    Completed both the functions of Array_Arranging_Function.c for array manupulation

--- Array_Arranging_Function.c
+++ Array_Arranging_Function.c
@@ -1,7 +1,19 @@
+int shortestNumberFinder(int arr[], int size);
+int highestNumberFinder(int arr[], int size);
+
 // Ascending order arranger
-void ascendingOrderArranger(int arr[], int size, int *ptr) // The third argument is the address of the base element of the new array where we will store the arranged numbers
+void ascendingOrderArranger(int arr[], int size, int new[]) // The third argument is the address of the base element of the new array where we will store the arranged numbers
 {
-    ;
+    for (int i = 0; i < size; i++)
+    {
+        int j = 0;
+        new[i] = shortestNumberFinder(arr, size);
+        while (arr[j] != new[i]) // For finding the position of the shortest number in old array
+        {
+            ++j;
+        }
+        arr[j] = highestNumberFinder(arr, size) + 1; // Replacing the smallest number with the number greater than the greatest
+    }
 }
 
 // Shortest Number Finder
@@ -18,9 +30,18 @@ int shortestNumberFinder(int arr[], int size)
     return temp;
 }
 // Descending order arranger
-void descendingOrderArranger(int arr[], int size, int *ptr) // The third argument is the address of the base element of the new array where we will store the arranged numbers
+void descendingOrderArranger(int arr[], int size, int new[]) // The third argument is the address of the base element of the new array where we will store the arranged numbers
 {
-    ;
+    for (int i = 0; i < size; i++)
+    {
+        int j = 0;
+        new[i] = highestNumberFinder(arr, size);
+        while (arr[j] != new[i]) // For finding the position of the largest number in old array
+        {
+            ++j;
+        }
+        arr[j] = shortestNumberFinder(arr, size) - 1; // Replacing the largest number with the number smaller than the smallest
+    }
 }
 
 // Highest Number Finder
--- Test_Program.c
+++ Test_Program.c
@@ -17,5 +17,17 @@ int main(int argc, char const *argv[])
     }
     printf("The shortest number is: %d\n", shortestNumberFinder(arr, number_of_terms));
     printf("The highest number is: %d\n", highestNumberFinder(arr, number_of_terms));
+
+    int new[number_of_terms];
+
+    // ascendingOrderArranger(arr, number_of_terms, new);
+    descendingOrderArranger(arr, number_of_terms, new);
+
+    for (int i = 0; i < number_of_terms; i++)
+    {
+        printf("%d\t", new[i]);
+    }
+    printf("\n");
+
     return 0;
 }

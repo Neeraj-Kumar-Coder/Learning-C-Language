bc534182738d3f82f346126d3da760b086466f3c
Neeraj Kumar <nk7075034@gmail.com>
2020-12-26 19:18:46 +0530

    modified morse code converter to convert in either form

--- International_Morse_Code.c
+++ International_Morse_Code.c
@@ -1,23 +1,78 @@
-#include <stdio.h>
+/*
+    Author : Neeraj Kumar
+    Purpose : Morse Code Project (FOR FUN)
+    Date : 26/12/2020
+    Time : 07:11 AM
+*/
+// This program can convert the TEXT TO MORSE CODE and also MORSE CODE TO TEXT
 
-void morseCode(char a);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void text_to_morse_code(char a);
+void morse_code_to_text(char str[]);
 
 int main(void)
 {
-    char a;
-    printf("Enter the message to convert it into Morse Code: ");
-    while ((a = getchar()) != '\n')
+    int choice;
+    printf("Enter the conversion you want to perform:\n1. Text to Morse Code\n2. Morse Code to Text\nINPUT : ");
+    scanf("%d", &choice);
+    getchar();
+
+    if (choice == 1)
     {
-        morseCode(a);
+        char a;
+        printf("Enter the message to convert it into Morse Code: ");
+        while ((a = getchar()) != '\n')
+        {
+            text_to_morse_code(a);
+        }
+    }
+
+    if (choice == 2)
+    {
+        char tempArr[6], a;
+        int i = 0, space = 0;
+
+        printf("Enter the encrypted message (MORSE CODE) : ");
+        while ((a = getchar()) != '\n')
+        {
+            if (a == '.' || a == '-')
+            {
+                tempArr[i++] = a;
+                space = 0;
+            }
+            else if (a == ' ')
+            {
+                tempArr[i] = '\0';
+                space++;
+                morse_code_to_text(tempArr);
+                i = 0;
+            }
+            else
+            {
+                putchar(a);
+            }
+
+            if (space > 1)
+            {
+                printf(" ");
+                space = 0;
+                i = 0;
+            }
+        }
+        tempArr[i] = '\0';
+        morse_code_to_text(tempArr);
     }
     return 0;
 }
 
-void morseCode(char a)
+void text_to_morse_code(char a)
 {
     if (a == 'A' || a == 'a')
     {
-        printf(".-");
+        printf(".- ");
     }
     else if (a == 'B' || a == 'b')
     {
@@ -170,5 +225,153 @@ void morseCode(char a)
     else
     {
         putchar(a);
+    }
+}
+
+void morse_code_to_text(char str[])
+{
+    if (strcmp(str, ".-") == 0)
+    {
+        printf("a");
+    }
+    else if (strcmp(str, "-...") == 0)
+    {
+        printf("b");
+    }
+    else if (strcmp(str, "-.-.") == 0)
+    {
+        printf("c");
+    }
+    else if (strcmp(str, "-..") == 0)
+    {
+        printf("d");
+    }
+    else if (strcmp(str, ".") == 0)
+    {
+        printf("e");
+    }
+    else if (strcmp(str, "..-.") == 0)
+    {
+        printf("f");
+    }
+    else if (strcmp(str, "--.") == 0)
+    {
+        printf("g");
+    }
+    else if (strcmp(str, "....") == 0)
+    {
+        printf("h");
+    }
+    else if (strcmp(str, "..") == 0)
+    {
+        printf("i");
+    }
+    else if (strcmp(str, ".---") == 0)
+    {
+        printf("j");
+    }
+    else if (strcmp(str, "-.-") == 0)
+    {
+        printf("k");
+    }
+    else if (strcmp(str, ".-..") == 0)
+    {
+        printf("l");
+    }
+    else if (strcmp(str, "--") == 0)
+    {
+        printf("m");
+    }
+    else if (strcmp(str, "-.") == 0)
+    {
+        printf("n");
+    }
+    else if (strcmp(str, "---") == 0)
+    {
+        printf("o");
+    }
+    else if (strcmp(str, ".--.") == 0)
+    {
+        printf("p");
+    }
+    else if (strcmp(str, "--.-") == 0)
+    {
+        printf("q");
+    }
+    else if (strcmp(str, ".-.") == 0)
+    {
+        printf("r");
+    }
+    else if (strcmp(str, "...") == 0)
+    {
+        printf("s");
+    }
+    else if (strcmp(str, "-") == 0)
+    {
+        printf("t");
+    }
+    else if (strcmp(str, "..-") == 0)
+    {
+        printf("u");
+    }
+    else if (strcmp(str, "...-") == 0)
+    {
+        printf("v");
+    }
+    else if (strcmp(str, ".--") == 0)
+    {
+        printf("w");
+    }
+    else if (strcmp(str, "-..-") == 0)
+    {
+        printf("x");
+    }
+    else if (strcmp(str, "-.--") == 0)
+    {
+        printf("y");
+    }
+    else if (strcmp(str, "--..") == 0)
+    {
+        printf("z");
+    }
+    else if (strcmp(str, "-----") == 0)
+    {
+        printf("0");
+    }
+    else if (strcmp(str, ".----") == 0)
+    {
+        printf("1");
+    }
+    else if (strcmp(str, "..---") == 0)
+    {
+        printf("2");
+    }
+    else if (strcmp(str, "...--") == 0)
+    {
+        printf("3");
+    }
+    else if (strcmp(str, "....-") == 0)
+    {
+        printf("4");
+    }
+    else if (strcmp(str, ".....") == 0)
+    {
+        printf("5");
+    }
+    else if (strcmp(str, "-....") == 0)
+    {
+        printf("6");
+    }
+    else if (strcmp(str, "--...") == 0)
+    {
+        printf("7");
+    }
+    else if (strcmp(str, "---..") == 0)
+    {
+        printf("8");
+    }
+    else if (strcmp(str, "----.") == 0)
+    {
+        printf("9");
     }
 }
